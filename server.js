fcb78705c0bedc249f5dfedcbe0767da967aa952
Brad Hodge <bradley_hodge@hotmail.com>
2023-02-01 19:04:22 -0500

    initial commit

--- server.js
+++ server.js
@@ -4,35 +4,27 @@
  * Server API file for novarya-creations
  */
 require('dotenv').config();
-
-const host = process.env.DB_HOST;
-const user = process.env.DB_USER;
-const pass = process.env.DB_PASS;
-const database = process.env.DB_DATABASE;
+const credentials = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    pass: process.env.DB_PASS,
+    database: process.env.DB_DATABASE,
+}
 
 const express = require('express');
-const bcrypt = require('bcrypt');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: host,
-    user: user,
-    password: pass,
-    database: database,
-});
-connection.connect();
-
-
-const cors = require('cors');
-app.use(cors({origin: ['https://www.novaryacreations.ca', 'http://localhost:3000']}));
-
-
-app.get('/users', (req, res) => {
-    connection.query('select * from users', (err, result) => {
-        res.send(result);
-    });
+    host: credentials.host,
+    user: credentials.user,
+    password: credentials.pass,
+    database: credentials.database,
+})
+connection.connect(err => {
+    if (err) throw err;
+    console.log('database connected')
 })
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
