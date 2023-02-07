e2564fe30e6e128465bbf0747f171c24f844d06c
Brad Hodge <bradley_hodge@hotmail.com>
2023-02-06 22:54:50 -0500

    added to whitelist

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:3000',
-    'https://novaryacreations.ca'
+    'https://www.novaryacreations.ca'
 ];
 
 module.exports = allowedOrigins;
